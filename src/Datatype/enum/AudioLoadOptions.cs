e81b6e5395a31274e4a17f0de5f50e6ecf37a448
greycsont <RuiD2019@outlook.com>
2026-03-15 01:06:02 +0000

    chore: split AudioPlayOptions and AuioLoadOptions to two files

---
+++ src/Datatype/enum/AudioLoadOptions.cs
@@ -0,0 +1,7 @@
+namespace GreyAnnouncer;
+
+public enum AudioLoadOptions
+{
+    Load_then_Play = 0,
+    Preload_and_Play = 1
+}
--- src/Datatype/enum/AudioPlayOptions.cs
+++ src/Datatype/enum/AudioPlayOptions.cs
@@ -5,9 +5,3 @@ public enum AudioPlayOptions
     Override_Last = 0,
     Independent = 1
 }
-
-public enum AudioLoadOptions
-{
-    Load_then_Play = 0,
-    Preload_and_Play = 1
-}
